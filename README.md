968e1e4d1e056c2a174b5b69bc90a17e462093c9
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-10-26 00:40:17 +0400

    Implement basic REPL

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # simple-query-engine
+
 Simple query engine (currently only for a CSV based dataset loaded into memory)
+
+## Installing from sources
+
+```bash
+cargo install --path .
+```
+
+## Running locally
+
+```bash
+simple-query-engine {path_to_csv_file}
+```
+
+Queries can be typed in the open interactive REPL session.
+
+For example using the CSV file in the `examples/data` directory:
+
+```bash
+simple-query-engine ./examples/data/input.csv
+```
+
+The following queries might be executed in the interactive REPL session:
+
+```bash
+PROJECT city_name, country, dominant_language FILTER population_size > 2000000
+```
+
+```bash
+PROJECT city_name, population_size FILTER dominant_language = "German"
+```
+
+## Development
+
+### Running tests
+
+```bash
+cargo test
+```
+
+### Running the engine
+
+```bash
+cargo run -- ./examples/data/input.csv
+```
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,53 @@
-use anyhow::{Result, Error};
+use anyhow::{anyhow, Result, Error};
 use std::fs::File;
+use std::{env, process};
+use std::io::{self, Write};
 use simple_query_engine::{Query, Table};
 
-fn main() -> Result<(), Error> {
-    let file_path = "./examples/data/input.csv";
-    let query = Query::parse("PROJECT city_name, country, dominant_language FILTER population_size > 2000000")?;
-    let file = File::open(file_path)?;
-    let mut reader = csv::Reader::from_reader(file);
-    let table = Table::load_from(&mut reader)?;
-    let indexed_table = table.build_indices()?;
-    let result_set = simple_query_engine::execute(&query, &indexed_table).unwrap();
-
-    let header = query.column_names.join(",");
-    let header_separator = "-".repeat(header.len());
-    print!("{}\n{}\n", header, header_separator);
-    for row in result_set.rows.iter() {
-        print!("{}\n", row);
+fn main() {
+    match run() {
+        Ok(_) => process::exit(0),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
     }
-    Ok(())
+}
+
+fn run() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Error: missing the CSV file path argument.");
+        eprintln!("Usage: {} <path_to_csv>", args[0]);
+        Err(anyhow!("CSV file path not provided"))
+    } else {
+        let file_path = &args[1];
+        let file = File::open(file_path)?;
+        let mut reader = csv::Reader::from_reader(file);
+        let table = Table::load_from(&mut reader)?;
+        let indexed_table = table.build_indices()?;
+
+        loop {
+            let stdin = io::stdin();
+            let mut input = String::new();
+            print!("> ");
+            io::stdout().flush()?;
+            stdin.read_line(&mut input)?;
+
+            let query = Query::parse(&input)?;
+
+            match simple_query_engine::execute(&query, &indexed_table) {
+                Ok(result_set) => {
+                    let header = query.column_names.join(",");
+                    let header_separator = "-".repeat(header.len());
+                    print!("{}\n{}\n", header, header_separator);
+                    for row in result_set.rows.iter() {
+                        println!("{}", row);
+                    }
+                },
+                Err(err) =>
+                    eprintln!("Query execution error: {}", err)
+            }
+        }
+    }
 }
